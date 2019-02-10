4c3fa1d8c18d858f6f475e0adcd6c7e3d38a3bca
Artur Minasjan <minasjan@gmail.com>
2019-02-10 15:41:25 +0200

    OneHourLamps unit tests added

--- UnitTests/TimeConverterTests.cs
+++ UnitTests/TimeConverterTests.cs
@@ -32,8 +32,8 @@ namespace UnitTests
         {
             for (int i = 0; i < 5; i++)
             {
-                Assert.AreEqual("OOOO", timeConverter.FiveHoursLamps(String.Format("0{0}:00:00",
-                         i)));
+                Assert.AreEqual("OOOO", timeConverter.FiveHoursLamps(String.Format("{0}:00:00",
+                         '0' + i.ToString())));
             }
         }
 
@@ -42,8 +42,8 @@ namespace UnitTests
         {
             for (int i = 5; i < 10; i++)
             {
-                Assert.AreEqual("ROOO", timeConverter.FiveHoursLamps(String.Format("0{0}:00:00",
-                         i)));
+                Assert.AreEqual("ROOO", timeConverter.FiveHoursLamps(String.Format("{0}:00:00",
+                          '0' + i.ToString())));
             }
         }
 
@@ -52,7 +52,7 @@ namespace UnitTests
         {
             for (int i = 10; i < 15; i++)
             {
-                Assert.AreEqual("RROO", timeConverter.FiveHoursLamps(String.Format("0{0}:00:00",
+                Assert.AreEqual("RROO", timeConverter.FiveHoursLamps(String.Format("{0}:00:00",
                          i)));
             }
         }
@@ -62,7 +62,7 @@ namespace UnitTests
         {
             for (int i = 15; i < 20; i++)
             {
-                Assert.AreEqual("RRRO", timeConverter.FiveHoursLamps(String.Format("0{0}:00:00",
+                Assert.AreEqual("RRRO", timeConverter.FiveHoursLamps(String.Format("{0}:00:00",
                          i)));
             }
         }
@@ -70,10 +70,52 @@ namespace UnitTests
         [TestMethod]
         public void FiveHoursLampsAllOn()
         {
-            for (int i = 20; i < 25; i++)
+            for (int i = 20; i < 24; i++)
             {
-                Assert.AreEqual("RRRR", timeConverter.FiveHoursLamps(String.Format("0{0}:00:00",
+                Assert.AreEqual("RRRR", timeConverter.FiveHoursLamps(String.Format("{0}:00:00",
                          i)));
+            }
+        }
+
+        [TestMethod]
+        public void OneHourLampsAllOff()
+        {
+            Assert.AreEqual("OOOO", timeConverter.OneHourLamps("00:00:00"));
+            Assert.AreEqual("OOOO", timeConverter.OneHourLamps("05:00:00"));
+            Assert.AreEqual("OOOO", timeConverter.OneHourLamps("10:00:00"));
+            Assert.AreEqual("OOOO", timeConverter.OneHourLamps("15:00:00"));
+            Assert.AreEqual("OOOO", timeConverter.OneHourLamps("20:00:00"));
+        }
+
+        [TestMethod]
+        public void OneHourLampsOnBeforeTen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 5 == 1)
+                    Assert.AreEqual("ROOO", timeConverter.OneHourLamps(String.Format("{0}:00:00", '0' + i.ToString())));
+                else if (i % 5 == 2)
+                    Assert.AreEqual("RROO", timeConverter.OneHourLamps(String.Format("{0}:00:00", '0' + i.ToString())));
+                else if (i % 5 == 3)
+                    Assert.AreEqual("RRRO", timeConverter.OneHourLamps(String.Format("{0}:00:00", '0' + i.ToString())));
+                else if (i % 5 == 4)
+                    Assert.AreEqual("RRRR", timeConverter.OneHourLamps(String.Format("{0}:00:00", '0' + i.ToString())));
+            }
+        }
+
+        [TestMethod]
+        public void OneHourLampsOnAfterTen()
+        {
+            for (int i = 10; i < 24; i++)
+            {
+                if (i % 5 == 1)
+                    Assert.AreEqual("ROOO", timeConverter.OneHourLamps(String.Format("{0}:00:00", i)));
+                else if (i % 5 == 2)
+                    Assert.AreEqual("RROO", timeConverter.OneHourLamps(String.Format("{0}:00:00", i)));
+                else if (i % 5 == 3)
+                    Assert.AreEqual("RRRO", timeConverter.OneHourLamps(String.Format("{0}:00:00", i)));
+                else if (i % 5 == 4)
+                    Assert.AreEqual("RRRR", timeConverter.OneHourLamps(String.Format("{0}:00:00", i)));
             }
         }
     }
